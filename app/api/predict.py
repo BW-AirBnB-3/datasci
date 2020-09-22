57a80ed109ce3128b6e609a1952defccf4ccc0d7
David Xu <dxu@pop-os.localdomain>
2020-09-22 08:25:17 -0700

    Modified predict.py

--- app/api/predict.py
+++ app/api/predict.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import joblib
 
 from fastapi import APIRouter
 import pandas as pd
@@ -12,19 +13,19 @@ router = APIRouter()
 class Item(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    x1: float = Field(..., example=3.14)
-    x2: int = Field(..., example=-42)
-    x3: str = Field(..., example='banjo')
+    borough: str = Field(..., example = 'Manhattan')
+    neighbourhood: str = Field(..., example = 'Midtown')
+    room_type: str = Field(..., example = 'Entire home/apt')
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
-    @validator('x1')
-    def x1_must_be_positive(cls, value):
-        """Validate that x1 is a positive number."""
-        assert value > 0, f'x1 == {value}, must be > 0'
-        return value
+    # @validator('x1')
+    # def x1_must_be_positive(cls, value):
+    #     """Validate that x1 is a positive number."""
+    #     assert value > 0, f'x1 == {value}, must be > 0'
+    #     return value
 
 
 @router.post('/predict')
@@ -47,9 +48,56 @@ async def predict(item: Item):
 
     X_new = item.to_df()
     log.info(X_new)
-    y_pred = random.choice([True, False])
-    y_pred_proba = random.random() / 2 + 0.5
+    
+    # Create all the dictionaries
+    df = pd.read_csv('./assets/listings.csv')
+    
+    def create_dict(borough: str):
+        select = df[df['borough'] == str]
+        unique = select['neighbourhood'].unique()
+        dct = {}
+        for element in unique:
+            dct['Bronx'] = element
+        
+        return dct
+    
+    bronx_dict = create_dict('Bronx')
+    brooklyn_dict = create_dict('Brooklyn')
+    manhattan_dict = create_dict('Manhattan')
+    queens_dict = create_dict('Queens')
+    staten_island_dict = create_dict('Staten Island')
+    
+    # Encode
+    blist1 = pd.factorize(df['borough'], sort = True)[1]
+    borough_dict = {}
+    for i in range(len(blist1)):
+        borough_dict[blist1[i]] = i
+    
+    nlist1 = pd.factorize(df['neighbourhood'], sort = True)[1]
+    neighbourhood_dict = {}
+    for i in range(len(nlist1)):
+        neighbourhood_dict[nlist1[i]] = i
+    
+    rlist1 = pd.factorize(df['room_type'], sort = True)[1]
+    room_type_dict = {}
+    for i in range(len(rlist1)):
+        room_type_dict[rlist1[i]] = i
+    
+    blist2 = pd.factorize(df['borough'], sort = True)[0]
+    X_new['borough'] = blist2
+    
+    nlist2 = pd.factorize(df['neighbourhood'], sort = True)[0]
+    X_new['neighbourhood'] = nlist2
+    
+    rlist2 = pd.factorize(df['room_type'], sort = True)[0]
+    X_new['room_type'] = rlist2
+    
+    # Predict
+    model = joblib.load('./assets/model.joblib')
+    
+    y_pred = model.predict(X_new)
     return {
-        'prediction': y_pred,
-        'probability': y_pred_proba
+        'prediction': y_pred
     }
+
+
--- app/api/viz.py
+++ app/api/viz.py
@@ -1,54 +1,25 @@
-from fastapi import APIRouter, HTTPException
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objs as gobj
+from plotly.offline import iplot
 
-router = APIRouter()
+path = './listings.csv'
 
+df = pd.read_csv(path)
 
-@router.get('/viz/{statecode}')
-async def viz(statecode: str):
-    """
-    Visualize state unemployment rate from [Federal Reserve Economic Data](https://fred.stlouisfed.org/) 📈
-    
-    ### Path Parameter
-    `statecode`: The [USPS 2 letter abbreviation](https://en.wikipedia.org/wiki/List_of_U.S._state_and_territory_abbreviations#Table) 
-    (case insensitive) for any of the 50 states or the District of Columbia.
+cols = ['neighbourhood_group','price']
 
-    ### Response
-    JSON string to render with [react-plotly.js](https://plotly.com/javascript/react/)
-    """
+data = dict(type = 'choropleth',
+            locations = df['neighbourhood_group'],
+            locationmode = 'USA-states',
+            autocolorscale = False,
+            colorscale = 'RdBu',
+            text= df['neighbourhood_group'],
+            z=df['price'],
+            marker = dict(line = dict(color = 'rgb(255,255,255)',width = 1)),
+            colorbar = {'title':'Colorbar Title','len':0.25,'lenmode':'fraction'})
 
-    # Validate the state code
-    statecodes = {
-        'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas', 
-        'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 
-        'DE': 'Delaware', 'DC': 'District of Columbia', 'FL': 'Florida', 
-        'GA': 'Georgia', 'HI': 'Hawaii', 'ID': 'Idaho', 'IL': 'Illinois', 
-        'IN': 'Indiana', 'IA': 'Iowa', 'KS': 'Kansas', 'KY': 'Kentucky', 
-        'LA': 'Louisiana', 'ME': 'Maine', 'MD': 'Maryland', 
-        'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota', 
-        'MS': 'Mississippi', 'MO': 'Missouri', 'MT': 'Montana', 
-        'NE': 'Nebraska', 'NV': 'Nevada', 'NH': 'New Hampshire', 
-        'NJ': 'New Jersey', 'NM': 'New Mexico', 'NY': 'New York', 
-        'NC': 'North Carolina', 'ND': 'North Dakota', 'OH': 'Ohio', 
-        'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 
-        'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 
-        'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VT': 'Vermont', 
-        'VA': 'Virginia', 'WA': 'Washington', 'WV': 'West Virginia', 
-        'WI': 'Wisconsin', 'WY': 'Wyoming'
-    }
-    statecode = statecode.upper()
-    if statecode not in statecodes:
-        raise HTTPException(status_code=404, detail=f'State code {statecode} not found')
+layout = dict(geo = dict(scope='usa'))
 
-    # Get the state's unemployment rate data from FRED
-    url = f'https://fred.stlouisfed.org/graph/fredgraph.csv?id={statecode}UR'
-    df = pd.read_csv(url, parse_dates=['DATE'])
-    df.columns = ['Date', 'Percent']
+ny_map = gobj.Figure(data = [data],layout = layout)
 
-    # Make Plotly figure
-    statename = statecodes[statecode]
-    fig = px.line(df, x='Date', y='Percent', title=f'{statename} Unemployment Rate')
-
-    # Return Plotly figure as JSON string
-    return fig.to_json()
+iplot(ny_map)
