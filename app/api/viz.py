01c835695f5867c4069dff063f4acb0ca4c4bf47
David Xu <dxu@pop-os.localdomain>
2020-09-23 08:33:31 -0700

    add router for viz

--- app/api/viz.py
+++ app/api/viz.py
@@ -2,24 +2,26 @@ import pandas as pd
 import plotly.graph_objs as gobj
 from plotly.offline import iplot
 
-path = './assets/listings.csv'
+@router.get('/viz/{statecode}')
+async def viz(statecode: str):
+    path = './assets/listings.csv'
 
-df = pd.read_csv(path)
+    df = pd.read_csv(path)
 
-cols = ['neighbourhood_group','price']
+    cols = ['neighbourhood_group','price']
 
-data = dict(type = 'choropleth',
-            locations = df['neighbourhood_group'],
-            locationmode = 'USA-states',
-            autocolorscale = False,
-            colorscale = 'RdBu',
-            text= df['neighbourhood_group'],
-            z=df['price'],
-            marker = dict(line = dict(color = 'rgb(255,255,255)',width = 1)),
-            colorbar = {'title':'Colorbar Title','len':0.25,'lenmode':'fraction'})
+    data = dict(type = 'choropleth',
+                locations = df['neighbourhood_group'],
+                locationmode = 'USA-states',
+                autocolorscale = False,
+                colorscale = 'RdBu',
+                text= df['neighbourhood_group'],
+                z=df['price'],
+                marker = dict(line = dict(color = 'rgb(255,255,255)',width = 1)),
+                colorbar = {'title':'Colorbar Title','len' 0.25,'lenmode':'fraction'})
 
-layout = dict(geo = dict(scope='usa'))
+    layout = dict(geo = dict(scope='usa'))
 
-ny_map = gobj.Figure(data = [data],layout = layout)
+    ny_map = gobj.Figure(data = [data],layout = layout)
 
-iplot(ny_map)
+    iplot(ny_map)
